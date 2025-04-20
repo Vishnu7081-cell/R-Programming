e1580bc3929638e67b3c1e63b123be89a241a732
Vishnu Gopinath <vishnugopi7081@gmail.com>
2025-04-21 00:05:04 +0530

    Update linear(001).R

--- linear(001).R
+++ linear(001).R
@@ -1,6 +1,9 @@
+# Step 1: Corrected equal-length vectors
 height <- c(180, 175, 164, 178, 155, 190)
 weight<- c(90, 68, 55, 60, 70, 96)
 
+# Step 2: Build the regression model
 relation <- lm(weight ~ height)
 
+# Step 3: Predict weight for height = 180
 predict(relation, data.frame(height=180))
